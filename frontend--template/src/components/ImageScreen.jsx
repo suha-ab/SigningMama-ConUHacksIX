b0419932a1b18206caa13f84b81529672d105b19
suha-ab <88583460+suha-ab@users.noreply.github.com>
2025-02-01 23:22:16 -0500

    made components

---
+++ frontend--template/src/components/ImageScreen.jsx
@@ -0,0 +1,5 @@
+export default function ImageScreen() {
+    return(
+        <div className="bg-black w-16 h-16"></div>
+    )
+}
---
+++ frontend--template/src/components/InstructionBar.jsx
@@ -0,0 +1,5 @@
+export default function InstructionBar() {
+    return(
+        <div className="bg-black w-16 h-16"></div>
+    )
+}
